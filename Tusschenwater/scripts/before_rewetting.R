6799f9be38f83a71be92ca24a87b841c1afcc23e
merijnslagter <merijnslagter@gmail.com>
2018-04-16 16:13:31 +0200

    Commit basic script to clip raster files to chosen area

---
+++ Tusschenwater/scripts/before_rewetting.R
@@ -0,0 +1,25 @@
+
+setwd("/home/merijn/Documents/Nijmegen_Stu_As/Project_Vegetation_Proxy/R/Tusschenwater")
+
+require(rgdal)
+require(rgeos)
+require(raster)
+
+twater <- readOGR("source/Tusschenwater_boundaries","Tusschenwater_boundaries")
+peatthick <- raster("source/veenraster.tif")
+gvgnow <- raster("source/gvghuidig.tif")
+gvgfuture <- raster("source/gvgplan.tif")
+
+gvgnow_crop <- crop(gvgnow, twater)
+gvgnow_mask <- mask(gvgnow, twater)
+
+gvgfuture_crop <- crop(gvgfuture, twater)
+gvgfuture_mask <- mask(gvgfuture, twater)
+
+peatthick_crop <- crop(peatthick, twater)
+peatthick_mask <- mask(peatthick, twater)
+
+
+writeRaster(gvgnow_mask,"inter/gvgnow_mask.tif", datatype='FLT4S', overwrite=TRUE)
+writeRaster(gvgfuture_mask,"inter/gvgfuture_mask.tif", datatype='FLT4S', overwrite=TRUE)
+writeRaster(peatthick_mask,"inter/peatthick_mask.tif", datatype='FLT4S', overwrite=TRUE)

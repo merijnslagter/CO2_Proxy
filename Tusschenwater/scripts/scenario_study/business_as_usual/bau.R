ecd2aeb48c1066f8e18499c6888ddf0e6f37c909
merijnslagter <merijnslagter@gmail.com>
2018-04-20 10:16:27 +0200

    business as usual update

--- Tusschenwater/scripts/scenario_study/business_as_usual/bau.R
+++ Tusschenwater/scripts/scenario_study/business_as_usual/bau.R
@@ -14,31 +14,32 @@ setwd("/home/merijn/Documents/Nijmegen_Stu_As/Project_Vegetation_Proxy/R/Tussche
 ## Laad bestanden
 
 peatthick <- raster("inter/peatthick_mask.tif")
-landuse <- raster("inter/gvgfuture_mask.tif")
+landuse <- readOGR("source/landuse_with_factors","landuse_with_factors")
 
-## Change land use to glg
+## Rasterize landuse
 
-conversiontable <- 
-glg <- 
+co2 <- rasterize(landuse, peatthick, field = "CO2")
 
+## Exclude peat shallower than 30 cm
+
+for(i in 1:length(peatthick)) {
+  if (!is.na(peatthick[i])) {
+    if (peatthick[i] < 30) {peatthick[i] <- NA}
+  }
+}
 
 ## calculate co2 emissies per cell
 
-co2endraster <- peatthick
-co2endraster[!is.na(co2endraster)][] <- 1
-co2sumraster <- co2endraster
-co2sumraster <- co2sumraster *30 
-
-for (i in 1:length(peatthick)) {
-  if(!is.na(peatthick[i]) & !is.na(glg[i])){
-  tabel <- co2_emissies_vernatting(glg[i],peatthick[i])
-  co2end <- tabel[1,2]
-  co2endraster[i] <- co2end
-  co2sum <- tabel[2,2]
-  co2sumraster[i] <- co2sum
-  }}
+co2sumraster <- peatthick
+co2sumraster[!is.na(co2sumraster)][] <- 1
+co2sumraster <- co2sumraster * co2 * 30
 
 ## Calculate average
 
-co2som <- cellStats(co2sumraster, stat='mean', na.rm=TRUE)
-co2eind <- cellStats(co2endraster, stat='mean', na.rm=TRUE)
+co2sum <- cellStats(co2sumraster, stat='mean', na.rm=TRUE)
+
+co2sumshape <- rasterToPolygons(co2sumraster, dissolve = T)
+
+co2sumshapearea <- gArea(co2sumshape) / 10000
+
+co2totalbau <- co2sum * co2sumshapearea
